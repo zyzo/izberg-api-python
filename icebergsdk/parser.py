79e3f5b7e6d6b9662fd58e3ec47eb186bb9da659
Florian Poullin <floriandiud@yahoo.fr>
2014-09-08 15:21:34 +0200

    Remove useless code

--- icebergsdk/parser.py
+++ icebergsdk/parser.py
@@ -55,14 +55,7 @@ class XMLParser(object):
             for dc in map(self. etree_to_dict, children):
                 for k, v in dc.iteritems():
                     dd[k].append(v)
-            # try:
-            d  = {
-                t.tag: {
-                    k: v[0] if len(v) == 1 else v for k, v in dd.iteritems()
-                }
-            }
-            # except SyntaxError:
-            # logger.warn("d = { t.tag: {k:v[0] if len(v) == 1 else v for k, v in dd.iteritems()} } SyntaxError")
+
             if not d.has_key(t.tag) or d[t.tag] == None:
                 d[t.tag] = {}
 
