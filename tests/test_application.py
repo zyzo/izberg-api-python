28f0b09f8f04ec56c13415a39ef75a5fbdae6214
Quentin <quentin@modizy.com>
2014-09-19 12:17:41 +0200

    TestApplication now uses setUp/tearDownClass

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from helper import IcebergUnitTestCase
+from helper import IcebergUnitTestCase, get_api_handler
 from icebergsdk.api import IcebergAPI
 from icebergsdk.exceptions import IcebergClientUnauthorizedError
 
 
 
 class TestApplication(IcebergUnitTestCase):
+
+
+    @classmethod
+    def setUpClass(cls):
+        cls.my_context_dict = {}
+        cls._objects_to_delete = []
+
     def test_01_create(self, namespace=None, name=None, contact_user=None):
         """
         Test Create an Application
@@ -25,26 +32,20 @@ class TestApplication(IcebergUnitTestCase):
 
         new_application = self.create_application(**data)
 
-        self.api_handler._objects_to_delete.append(new_application)
+        self._objects_to_delete.append(new_application)
+        self.my_context_dict["new_application"] = new_application
 
         return new_application
 
-    def test_02_delete(self, application=None):
-        """
-        Test Delete an Application
-        """
-        if application is None:
-            application = self.test_01_create()
-        application.delete()
-
-    def test_03_sso_read(self):
+    def test_02_sso_read(self):
         """
         Test SSO Read an Application
         - Fetch the application secret key
         - SSO Login on this application
         - Assert authorized read detail by contact_user
         """
-        new_application = self.test_01_create()
+        self.direct_login_user_1()
+        new_application = self.my_context_dict["new_application"]
         previous_conf = self.api_handler.conf
         new_conf = previous_conf() ## here we instanciate the previous conf so that we can modify some values without changing the class values
         new_conf.ICEBERG_APPLICATION_SECRET_KEY = str(new_application.fetch_secret_key())
@@ -67,17 +68,31 @@ class TestApplication(IcebergUnitTestCase):
         self.api_handler.conf = previous_conf
 
 
-    def tearDown(self):
-        if hasattr(self.api_handler, "_objects_to_delete"):
-            self.direct_login_iceberg_staff()
-            for obj in self.api_handler._objects_to_delete:
+    def test_03_delete(self, application=None):
+        """
+        Test Delete an Application
+        """
+        self.direct_login_user_1()
+        self.my_context_dict["new_application"].delete()
+
+        if self.my_context_dict["new_application"] in self._objects_to_delete:
+            ## no need to delete it in tearDownClass if delete succeeded
+            self._objects_to_delete.remove(self.my_context_dict["new_application"])
+
+
+
+
+    @classmethod
+    def tearDownClass(cls):
+        if hasattr(cls, "_objects_to_delete"):
+            api_handler = get_api_handler()
+            api_handler.auth_user(username="staff_iceberg", email="staff@iceberg-marketplace.com", is_staff = True)
+
+            for obj in cls._objects_to_delete:
                 try:
-                    obj.delete()
+                    obj.delete(handler = api_handler)
                     # print "obj %s deleted" % obj
                 except:
                     pass
                     # print "couldnt delete obj %s" % obj
-
-        super(TestApplication, self).tearDown()
-
 
